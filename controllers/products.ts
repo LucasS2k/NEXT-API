7226c0f4156af8674e22229a76c55e1dd22dd412
lucass2k <lucasnahuel2000silva@gmail.com>
2023-10-07 09:14:50 -0300

    fix

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import Product, { IProduct } from "../models/product";
+import Product from "../models/product";
 export const getProducts = async (req: Request, res: Response) => {
   const products = await Product.find();
   res.status(200).json({ products });
